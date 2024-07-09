8c29fc235b8ad942e3c241a9ec80ab6282b02fd4
Hammad Afzaal <143577864+hammadafzaal06@users.noreply.github.com>
2024-07-09 12:00:36 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,8 +1,14 @@
 let age = null
 
-console.log(typeof age)
-console.log(typeof(age));
+// console.log(typeof age)
+// console.log(typeof(age));
 
 let valueInNum = Number(age)
-console.log(typeof valueInNum);
-console.log(valueInNum);
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+//*********Operations**********
+let value = 3
+let nagValue = -3
+
+console.log(nagValue);
