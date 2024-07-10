ca29618376b7751bc205f7ad18d93c60b0de7720
Hammad Afzaal <143577864+hammadafzaal06@users.noreply.github.com>
2024-07-10 07:38:17 +0000

    String Interpolations

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,36 @@
+// String Interpolation in JavaScript
+
+// const name= "Hammad";
+// const repoCount = 10;
+
+// console.log(`Hello my Name is ${name} and my RepoCount ${repoCount}`);
+
+
+// const name1 = new String("Hammad")
+
+// console.log(name1);
+// console.log(name1[4]);
+// console.log(name1.__proto__);
+// console.log(name1.toUpperCase());
+// console.log(name1.charAt(2));
+// console.log(name1.indexOf('d'));
+
+// const newString = name1.substring(-8, 4)
+// console.log(newString);
+
+// const anotherString = name1.slice(-8, 4)
+// console.log(anotherString);
+
+
+// const userName = "     ali    ";
+// console.log(userName);
+// console.log(userName.trim());
+// console.log(userName.trimStart());
+// console.log(userName.trimEnd());
+
+
+const url = "http//google.com/string%20interpolation";
+// console.log(url.replace('%20', '#')) 
+
+// console.log(url.includes('google')); 
+console.log(url.split(' '));
